2744ac972ac1ecb4b8e99660be6d476f793fb2ea
VIVEK BARANWAL <vbaranwal01@gmail.com>
2025-10-22 14:13:43 +0530

    Update BbitHotelBookingApplication.java

--- src/main/java/com/bbit/app/bbithotelbooking/BbitHotelBookingApplication.java
+++ src/main/java/com/bbit/app/bbithotelbooking/BbitHotelBookingApplication.java
@@ -1,5 +1,6 @@
 package com.bbit.app.bbithotelbooking;
 
+//UPDATED - import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
